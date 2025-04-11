6043e2d4ef7674ed04933107770f6f1658ba1612
fufex3934 <fufawakgari174@gmail.com>
2025-04-11 12:42:58 +0300

    add, delete operations

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,54 @@
-
-
+import TaskForm from "./components/TaskForm";
+import TaskList from "./components/TaskList";
+import TaskFilter from "./components/TaskFilter";
+import { useState } from "react";
 
 function App() {
-  
+  const [tasks, setTasks] = useState([]);
+  const [filter, setFilter] = useState("All");
 
+  //adding task
+  const addTask = (taskDescription) => {
+    const newTask = {
+      id: Date.now(),
+      description: taskDescription,
+      completed:  false,
+    };
+    setTasks([...tasks,newTask]);
+  };
+
+  //deleting task
+  const deleteTask = (id)=>{
+    setTasks(tasks.filter((task)=>task.id !==id));
+  };
+
+  //toggle task completion status
+  const toggleTaskStatus = (id)=>{
+    setTasks(
+      tasks.map((task)=>
+        task.id === id ? {...task,completed:!task.completed} : task
+      )
+    )
+  }
+
+  //filter task based on completion status
+  const filteredTasks = tasks.filter((task)=>{
+    if(filter === 'Completed'){
+      return task.completed
+    }
+    if(filter === 'Pending'){
+      return !task.completed
+    }
+    return true;
+  });
   return (
-    <>
-     <h1 className='text-red-500 underline'>Hello World</h1>
-    </>
-  )
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-center text-3xl font-bold mb-4">Task Tracker App</h1>
+      <TaskForm />
+      <TaskFilter />
+      <TaskList />
+    </div>
+  );
 }
 
-export default App
+export default App;
